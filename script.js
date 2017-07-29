e8a59bf19226d624bfce0aff8875d986e3ac6d69
Timothy Batty <tbatty7@gmail.com>
2017-07-29 11:44:19 -0400

    set up routing for cat and dog

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-var app = angular.module('myModule', ['ngRoute']);
+var app = angular.module('petModule', ['ngRoute']);
 
 
 app.config(function($routeProvider, $locationProvider){
 	$routeProvider
 	.when('/', 
 	{
-		controller: 'SimpleController',
-		templateURL: 'file path to html doc1'
+
+		templateURL: 'cat.html'
 	})
 	.when('/view2',
 	{
-		controller: 'SimpleController',
-		templateURL: 'second view - html doc2'
+		controller: 'controlDog',
+		templateURL: 'dog.html'
 	}
-	.otherwise({redirectTo: '/'});
+	.otherwise(template: '<h1>No Pets Here</h1>');
 	$locationProvider.hashPrefix('');
 })
 
-app.controller('myController', function($scope){
-	$scope.toDo = [{item: "mow lawn", completed: true}, {item: "feed cat", completed: false}, 
-	{item: "walk dog", completed: false}, {item: "do dishes", completed: false}];
-
-	$scope.addNewItem = function() {
-		$scope.toDo.push({item: $scope.newItem, completed: false});
-		$scope.newItem = "";
-	}
-
-	$scope.removeListItem = function() {
-		$scope.toDo.pop();
-		}
+app.controller('controlDog', function($scope){
+	
 })
 
